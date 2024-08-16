1a5baad2a93fe8d43f2eecf353389132c6d7c2d2
MrYasuo <buihai0706vn@gmail.com>
2024-08-16 11:17:13 +0700

    chore: Update treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,8 +2,9 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     tag = "v0.9.1",
-    opts = {
-      ensure_installed = {
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, {
         "javascript",
         "typescript",
         "css",
@@ -15,12 +16,27 @@ return {
         "sql",
         "vim",
         "lua",
-      },
-      query_linter = {
+        "html", -- Make sure HTML is installed
+      })
+
+      opts.query_linter = {
         enable = true,
         use_virtual_text = true,
         lint_events = { "BufWrite", "CursorHold" },
-      },
-    },
+      }
+
+      -- Use a function to set up filetype_to_parsername
+      opts.filetype = {
+        enable = true, -- This line is important
+        custom_mappings = {
+          xml = "html",
+        },
+      }
+
+      return opts
+    end,
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
   },
 }
